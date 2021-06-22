02066fe7e85d7e1b7f11df9dd2c524e6c94bf278
michail <backtrackpower@gmail.com>
2021-06-22 20:39:41 +0300

    TDD
    unit test practice

---
+++ Django/recipe-app-api/app/app/calc.py
@@ -0,0 +1,11 @@
+
+
+def add(x, y):
+    """Add two number together"""
+    return x + y
+
+
+def substract(x, y):
+    """Substract two numbers TDD"""
+    # pass
+    return x - y
---
+++ Django/recipe-app-api/app/app/test_1.py
@@ -0,0 +1,18 @@
+import unittest
+
+from app.calc import add, substract
+
+
+class Test_test_1_calc(unittest.TestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together TDD"""
+        self.assertEqual(add(5, 5), 10)
+
+    def test_substract_numbers(self):
+        """Test that values are substracted and returned TDD"""
+        self.assertEqual(substract(5, 4), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
