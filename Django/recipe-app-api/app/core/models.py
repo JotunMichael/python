9e76374f6c3b9bebf1144ee2b4b5489b62cc5029
michail <backtrackpower@gmail.com>
2021-07-27 12:48:17 +0300

    Added recipe model.

--- Django/recipe-app-api/app/core/models.py
+++ Django/recipe-app-api/app/core/models.py
@@ -62,3 +62,23 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+    # blank string if you omit on object creation CharField : blank=True
+    # null field different more complex
+    # null then must  check if its blank or not or has a value in TDD
+
+    def __str__(self):
+        return self.title
