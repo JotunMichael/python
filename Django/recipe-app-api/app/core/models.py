a281da417a03b931324d6881073fab136c83286e
michail <backtrackpower@gmail.com>
2021-07-27 11:43:31 +0300

    Added list ingredients feature.

--- Django/recipe-app-api/app/core/models.py
+++ Django/recipe-app-api/app/core/models.py
@@ -46,5 +46,19 @@ class Tag(models.Model):
         on_delete=models.CASCADE,
     )
 
+    # string representation of the model
+    # toString same as Java for print?
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient to be used in a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
     def __str__(self):
         return self.name
