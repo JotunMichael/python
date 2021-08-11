70e1bce1668ee5a64e8068f98ccba7235e22a3f8
michail <backtrackpower@gmail.com>
2021-08-11 07:25:55 +0300

    Added image field to recipe model.

--- Django/recipe-app-api/app/recipe/tests/test_recipe_api.py
+++ Django/recipe-app-api/app/recipe/tests/test_recipe_api.py
@@ -237,3 +237,44 @@ class PrivateRecipeApiTests(TestCase):
             # check if one value (expected) is in another value (actual)
             self.assertIn(ingredient1, ingredients)
             self.assertIn(ingredient2, ingredients)
+
+            # test already out of the box no need for updates (in modelViewSet)
+            # patch updates obj modified fields only leaving untouched others
+            # put overrides omitted info to obj
+            def test_partial_update_recipe(self):
+                """Test updating a recipe with patch"""
+                recipe = sample_recipe(user=self.user)
+                recipe.tags.add(sample_tag(user=self.user))
+                # add new tag to already recipe created (replace/override)
+                new_tag = sample_tag(user=self.user, name='Curry')
+
+                payload = {'title': 'Chicken tikka', 'tags': [new_tag.id]}
+                url = detail_url(recipe.id)
+                self.client.patch(url, payload)
+
+                # var gets new updates from DB if changed
+                recipe.refresh_from_db()
+                self.assertEqual(recipe.title, payload['title'])
+                tags = recipe.tags.all()
+                self.assertEqual(len(tags), 1)
+                self.assertIn(new_tag, tags)
+
+            def test_full_update_recipe(self):
+                """Test updating a recipe with put"""
+                recipe = sample_recipe(user=self.user)
+                recipe.tags.add(sample_tag(user=self.user))
+
+                payload = {
+                    'title': 'Spaghetti carbonara',
+                    'time_minutes': 25,
+                    'price': 5.00
+                }
+                url = detail_url(recipe.id)
+                self.client.put(url, payload)
+
+                recipe.refresh_from_db()
+                self.assertEqual(recipe.title, payload['title'])
+                self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+                self.assertEqual(recipe.price, payload['price'])
+                tags = recipe.tags.all()
+                self.assertEqual(len(tags), 0)
